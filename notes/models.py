638ce0fd47922712bf8d1cd01ad9d3b411e9e79e
giavassallo <gpv0218@gmail.com>
2026-03-03 06:27:43 -0500

    Update models.py

--- notes/models.py
+++ notes/models.py
@@ -1,10 +1,29 @@
+"""
+Models for Notes App
+
+Defines database schema for note storing
+"""
+
 from django.db import models
 
-# Create your models here.
+"""
+Respresents single note in app
+
+Attributes:
+    title(str): title of note
+    comtent(str): main text of note
+    Created_at(datetime): atuo timestamp when note is created
+"""
 class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+"""
+Return a readable string of the note
+
+Return:
+    str: title of note
+"""
     def __str__(self):
         return self.title
