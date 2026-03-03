2bd50708cd0ce0a7e6b2dd967896a42ab463b4f0
giavassallo <gpv0218@gmail.com>
2026-03-03 06:32:28 -0500

    Add docstring to NoteForm for clarity

--- notes/forms.py
+++ notes/forms.py
@@ -1,7 +1,24 @@
+"""
+Forms for Note App
+
+Defines forms used to create/update Note instances
+"""
 from django import forms
 from .models import Note
 
+"""
+Form for creating/updating objects
+
+Based on Note model and includes title and content fields
+"""
 class NoteForm(forms.ModelForm):
+    """
+    Meta config for NoteForm
+
+    Attributes:
+    model(model): model associated with form
+    fields(list): fields included in form
+    """
     class Meta:
         model = Note
         fields = ['title', 'content']
